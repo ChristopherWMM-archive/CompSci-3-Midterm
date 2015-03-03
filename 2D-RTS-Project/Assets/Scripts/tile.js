c011d288fe3aec70e610ebeeec42d272b6ab37b5
startwarfields <jigglymex@gmail.com>
2015-03-02 18:34:01 -0800

    Jonathan-V.01 Update
    Update from V.00 to V.01
    -tiles added
    -tiles change color when clicked on
    
    WIP:
    Changing Tags in runtime

---
+++ 2D-RTS-Project/Assets/Scripts/tile.js
@@ -0,0 +1,28 @@
+﻿
+
+var team1Mat: Material;
+
+var whichTeam: int;
+var thisTile: GameObject;
+
+// Team 1=1
+// Team 2=-1;
+function Start () {
+whichTeam=1;
+}
+
+function Update () {
+if(whichTeam==1)
+	thisTile.renderer.material.color=Color.blue;
+	
+if(whichTeam==-1)
+		thisTile.renderer.material.color=Color.red;
+
+}
+
+
+function OnMouseDown()
+{
+	whichTeam*=-1;
+	
+}

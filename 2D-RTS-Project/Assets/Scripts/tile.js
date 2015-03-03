7845c03323e1194d7ff3936e48776b6da9b9e07f
startwarfields <jigglymex@gmail.com>
2015-03-03 10:53:53 -0800

    V.05
    Added Units
    -Units can be selected then switched

--- 2D-RTS-Project/Assets/Scripts/tile.js
+++ 2D-RTS-Project/Assets/Scripts/tile.js
@@ -4,6 +4,9 @@ var team1Mat: Material;
 var mapType: Material;
 var whichTeam: int;
 var thisTile: GameObject;
+var inYield : boolean;
+var targetUnit: GameObject;
+var targetUnitScript;
 //We'll think of some fancy way to ID teams, 
 // Team 1=1
 // Team 2=-1;
@@ -12,31 +15,63 @@ var isSelected: boolean;
 var FortLevel: int;
 var UnitsStored :int;
 var morale: int;
+var maxUnits: int;
+var sendUnit: boolean;
+var UnitArray;
 function Start () {
 //All this will be pulled from a database later on. 
  whichTeam=1;
  FortLevel=1;
  UnitsStored=1000;
- morale=10.0;
+ //morale=10.0;
+ //Morale is defunct for now
  maxUnits=1000*FortLevel;
  isSelected=false;
+ inYield=false;
+ sendUnit=false;
 }
 
 function Update () {
 // Lol I'll look at the project I have that we made for this feature...later
 // if(Time.timeSinceLevelLoad % 60==59)
- // addTroops();
+
+UnitArray=GameObject.FindGameObjectsWithTag("selectedUnit");
+ 
+ 
+	if(!inYield && UnitsStored<maxUnits) 
+		{
+			 addTroops(); 	 
+		}
   
 	if(isSelected)
 	{
 	
 		if(Input.GetKeyDown(KeyCode.F))
 		{
-		   morale=morale-0.5;
+		  // morale=morale-0.5;
+		  //Morale is defunct for now
 		   UnitsStored=UnitsStored-50;
 		
 		}
-	
+		if(Input.GetKeyDown(KeyCode.G))
+		{
+		
+		whichTeam=-whichTeam;
+		}
+		var zi: int;
+		for( zi=0;zi<UnitArray.length;zi++)
+		{
+		 targetUnit=UnitArray[zi];
+		  targetUnitScript=(UnitArray[zi].GetComponent("unit"));
+		   
+	      if(sendUnit && targetUnitScript.getSelected())
+	      {
+	      targetUnit.transform.position= Vector3(thisTile.transform.position.x,targetUnit.transform.position.y,thisTile.transform.position.z);
+	      sendUnit=false;
+	      targetUnitScript.setSelected(false);
+	      }
+	     }
+	     
 	}
 	
 	if(whichTeam==1)
@@ -54,17 +89,19 @@ function Update () {
 		 	
 	}
 	
-	 	
+	
 }
 
 function addTroops() {
-yield WaitForSeconds(5);
+inYield=true;
+yield WaitForSeconds(3);
  UnitsStored+=10;
+ inYield=false;
 }
 
 function OnMouseDown()
 {
-	whichTeam*=-1;
+	sendUnit=true;
 	//No If tests in this function
 	
 	
---
+++ 2D-RTS-Project/Assets/Scripts/unit.js
@@ -0,0 +1,30 @@
+﻿#pragma strict
+var isSelected=true;
+var thisUnit : GameObject;
+function Start () {
+isSelected=false;
+thisUnit.tag="selectedUnit";
+}
+
+function Update () {
+
+ 
+ 	
+ 	
+ 	
+}
+
+function OnMouseDown() {
+isSelected=!isSelected;
+
+}
+
+function getSelected() {
+
+return(isSelected);
+}
+function setSelected(isSel: boolean)
+{
+
+ isSelected=isSel;
+} 

c95aebac992133530b37bbd8f22a18b4d471bbf4
OmniCypher <OmniCypher@yahoo.com>
2015-03-13 10:41:10 -0700

    Added Cursors for each Team

---
+++ 2D-RTS-Project/Assets/Scripts/cursorScript.js
@@ -0,0 +1,25 @@
+﻿var mouse : Vector2;
+var w : int = 32;
+var h : int = 32;
+var cursor : Texture2D;
+var cursorRed : Texture2D;
+var cursorBlue : Texture2D;
+     
+function Start(){
+     Screen.showCursor = false;
+}
+     
+function Update(){
+     mouse = new Vector2(Input.mousePosition.x, Screen.height - Input.mousePosition.y);
+     
+}
+     
+function OnGUI(){
+	var turn : int = GetComponent(gameMaster).whichTurn;
+	if(turn == -1)
+     	GUI.DrawTexture(new Rect(mouse.x - (w / 2), mouse.y - (h / 2), w, h), cursorRed);
+    else if(turn == 1)
+    	GUI.DrawTexture(new Rect(mouse.x - (w / 2), mouse.y - (h / 2), w, h), cursorBlue);
+    else
+    	GUI.DrawTexture(new Rect(mouse.x - (w / 2), mouse.y - (h / 2), w, h), cursor);
+}
